0a07ebbb035efc93a568e05cafdb6a17eb09b748
Roberto Angulo <rangulo1972@gmail.com>
2025-01-27 17:11:49 -0500

    correción de comando para ejecución de script.py

--- script.py
+++ script.py
@@ -1,2 +1,3 @@
 print('This is the final text')
-print('Realizando cambios')
+print('Realizando Cambios')
+print('Fallo en build#2 por comando errado al no especificar python3 para ejecuón de script.py')
